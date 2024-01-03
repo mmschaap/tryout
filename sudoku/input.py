4bc4b2b597131fd85305c536e2143a45db3789a3
M <martin.schaap@xs4all.nl>
2024-01-03 18:48:55 +0100

    Check duplicates in block.

--- sudoku/input.py
+++ sudoku/input.py
@@ -32,46 +32,46 @@ def get_input( sudoku ):
         #
         # 0 => [0][0] 
         # 2 => [0][2] 
-        # 7 => [3][2] 
+        # 7 => [2][1] 
         #
-        block_column = int( i / 3 )  
-        block_row = i - int( i / 3 ) * 3
+        block_row = int( i / 3 )
 
         for j in range( 0, 9 ):
-            cell_column = int( j / 3 )  
-            cell_row = j - int( j / 3 ) * 3
+            block_column = int( j / 3 )
+            cell_column = j - int( j / 3 ) * 3
+            cell_row = i - int( i / 3 ) * 3
 
-            sudoku[block_column][block_row][cell_column][cell_row] = parsed_line[j]  
+            sudoku[block_row][block_column][cell_row][cell_column] = parsed_line[j]  
 
     #
-    # Check for duplicates
+    # Check for duplicates in rows and columns
     #
     check_passed = True
 
     for i in range( 0, 9 ):
-        block_column_source = int( i / 3 )  
-        block_row_source = i - int( i / 3 ) * 3 
         for j in range( 0, 9 ):
-            cell_column_source = int( j / 3 )  
-            cell_row_source = j - int( j / 3 ) * 3
+            block_column_source = int( i / 3 )  
+            block_row_source =    int( j / 3 )
+            cell_column_source =  i - int( i / 3 ) * 3
+            cell_row_source =     j - int( j / 3 ) * 3
 
-            for k in range( i, 9-i ):
-                block_column = int( k / 3 )  
-                block_row = k - int( k / 3 ) * 3 
-                cell_column = cell_column_source
-                cell_row = ( block_row_source + k ) - int( ( block_row_source + k ) / 3 ) * 3
+            for k in range( j+1, 9-j ):
+                block_column = block_column_source
+                block_row =    int( k / 3 )
+                cell_column =  cell_column_source
+                cell_row =     k - int(  k / 3 ) * 3
 
-                if sudoku[block_column_source][block_row_source][cell_column_source][cell_row_source] != '.' and \
-                    sudoku[block_column_source][block_row_source][cell_column_source][cell_row_source] == \
-                    sudoku[block_column][block_row][cell_column][cell_row] and check_passed == True:
+                if sudoku[block_row_source][block_column_source][cell_row_source][cell_column_source] != '.' and \
+                    sudoku[block_row_source][block_column_source][cell_row_source][cell_column_source] == \
+                    sudoku[block_row][block_column][cell_row][cell_column] and check_passed == True:
                     print( "Inconsistent Sudoku specified." )
                     check_passed = False
 
             if check_passed == True:
-                for l in range( i, 9-i ):
+                for l in range( i+1, 9-i ):
                     block_column = int( l / 3 )  
-                    block_row = l - int( l / 3 ) * 3 
-                    cell_column = int( block_column_source + l / 3 )  
+                    block_row = block_row_source
+                    cell_column = l - int( l / 3 ) * 3
                     cell_row = cell_row_source
 
                     if sudoku[block_column_source][block_row_source][cell_column_source][cell_row_source] != '.' and \
@@ -80,3 +80,22 @@ def get_input( sudoku ):
                         print( "Inconsistent Sudoku specified." )
                         check_passed = False                        
                         
+    #
+    # Check for duplicates in a cell of 9 figures
+    #
+    cell_figures = ['.' for _ in range( 9 )]
+    check_passed = True
+                    
+    for i in range( 0, 3 ):
+        for j in range( 0, 3 ):
+            if check_passed == True:
+                for k in range( 0, 3 ):
+                    for l in range( 0, 3 ):
+                        cell_figures[ k * 3 + l ] = sudoku[i][j][k][l]
+
+                cell_figures.sort()
+
+                for m in range( 0, 8 ):
+                    if cell_figures[ m ] != '.' and cell_figures[ m ] == cell_figures[ m + 1 ] and check_passed == True:
+                        print( "Inconsistent Sudoku specified." )
+                        check_passed = False
--- sudoku/solver.py
+++ sudoku/solver.py
@@ -10,5 +10,3 @@ is_not = [[[[['X' for _ in range( 9 )] for _ in range( 3 )] for _ in range( 3 )]
 from input import get_input
 
 get_input( sudoku )
-
-print( sudoku )
