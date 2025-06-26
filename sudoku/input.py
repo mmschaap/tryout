f91889658449d08a250f305eae19eb56016f1f00
M <martin.schaap@xs4all.nl>
2025-06-26 22:15:39 +0200

    Solved by Github Copilot

--- sudoku/input.py
+++ sudoku/input.py
@@ -8,6 +8,17 @@ from helpers import getRightLineInput, hasDuplicatesInRowsOrColumns, hasDuplicat
 
 use_test_data = True
 
+def print_sudoku(sudoku):
+    for row in range(9):
+        line = []
+        block_row = row // 3
+        cell_row = row % 3
+        for col in range(9):
+            block_col = col // 3
+            cell_col = col % 3
+            line.append(sudoku[block_row][block_col][cell_row][cell_col])
+        print(" ".join(line))
+
 def get_input( sudoku ):
     if use_test_data == False:
         for i in range( 9 ):
@@ -25,3 +36,8 @@ def get_input( sudoku ):
     else:
         if hasDuplicatesInBlocks(sudoku):
             print( "Inconsistent Sudoku specified." )
+
+    # Display the sudoku before solving
+    print("Sudoku before solving:")
+    print_sudoku(sudoku)
+    
--- sudoku/process.py
+++ sudoku/process.py
@@ -4,5 +4,46 @@
 # Solve a Sudoku puzzle
 # by Martin Schaap
 #
+from input import print_sudoku
+
 def process_sudoku( sudoku, can_be ):
+    solve_sudoku(sudoku)
+ 
+    # Display the sudoku before solving
+    print("Sudoku after solving:")
+    print_sudoku(sudoku)
     return
+
+def is_valid(sudoku, block_row, block_col, cell_row, cell_col, num):
+    num = str(num)
+    # Check row
+    for bc in range(3):
+        for cc in range(3):
+            if sudoku[block_row][bc][cell_row][cc] == num:
+                return False
+    # Check column
+    for br in range(3):
+        for cr in range(3):
+            if sudoku[br][block_col][cr][cell_col] == num:
+                return False
+    # Check 3x3 block
+    for cr in range(3):
+        for cc in range(3):
+            if sudoku[block_row][block_col][cr][cc] == num:
+                return False
+    return True
+
+def solve_sudoku(sudoku):
+    for block_row in range(3):
+        for block_col in range(3):
+            for cell_row in range(3):
+                for cell_col in range(3):
+                    if sudoku[block_row][block_col][cell_row][cell_col] == ".":
+                        for num in range(1, 10):
+                            if is_valid(sudoku, block_row, block_col, cell_row, cell_col, num):
+                                sudoku[block_row][block_col][cell_row][cell_col] = str(num)
+                                if solve_sudoku(sudoku):
+                                    return True
+                                sudoku[block_row][block_col][cell_row][cell_col] = "."
+                        return False
+    return True
